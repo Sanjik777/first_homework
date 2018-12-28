ccbfb7c7b5be83331e4873681b0bb891b78aa810
Sanjik <Beiseev_193@mail.ru>
2018-12-29 02:04:11 +0600

    v1

---
+++ First_Homework/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace First_Homework
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string name = "Hello!";
+			Console.WriteLine(name);
+			Console.ReadLine();
+		}
+	}
+}
